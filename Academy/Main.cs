07f47da26a04bc6d39cded2e4d34b7ddb1a47d19
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-03-25 21:33:35 +0300

    DataTable Load()

--- Academy/Main.cs
+++ Academy/Main.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Management;
+using System.Configuration;
+
 namespace Academy
 {
 	public partial class Main : Form
@@ -15,6 +18,14 @@ namespace Academy
 		public Main()
 		{
 			InitializeComponent();
+
+			Connector connector = new Connector
+				(
+					ConfigurationManager.ConnectionStrings["PV_319_Import"].ConnectionString
+				);
+			//dgv - DataGridView
+			dgvStudents.DataSource = connector.Select("*", "Students");
+			//tabPageGroups.DataSource = connector.Select("*", "Groups");
 		}
 	}
 }
